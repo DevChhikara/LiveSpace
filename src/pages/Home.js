540e5a9608253915f2ad53c45a329ca1ed8ca797
KartikChawla09 <kartik.chawla.ug21@nsut.ac.in>
2023-07-16 19:44:10 +0530

    added hero section and updated navbar

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import "./Home.css";
+import Hero from "../Components/Hero";
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
