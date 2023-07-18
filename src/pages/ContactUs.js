1f1ae5585dee7c2bd10d3a5645c903e5dd76e379
KartikChawla09 <kartik.chawla.ug21@nsut.ac.in>
2023-07-18 18:08:09 +0530

    added contact form

---
+++ src/pages/ContactUs.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+import "./ContactUs.css";
+import ContatcForm from "../Components/ContatcForm";
+const ContactUs = () => {
+  return (
+    <div>
+      <Navbar />
+      <ContatcForm />
+      <Footer />
+    </div>
+  );
+};
+
+export default ContactUs;
