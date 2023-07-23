b6c651d5d6831d905c3641f87cbad7f7e1becce2
Dev Chhikara <devchhikara@Devs-MacBook-Air-2.local>
2023-07-23 19:41:30 +0530

    added services page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Home from "./pages/Home";
+import ContactUs from "./pages/ContactUs";
+import About from "./pages/About";
+import Services from "./pages/Services";
+
+function App() {
+  return (
+    <div>
+      {/* <ContactUs></ContactUs> */}
+      {/* <Home></Home> */}
+      {/* <About /> */}
+      <Services></Services>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/AboutServices.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import "./AboutServices.css"
+
+const AboutServices = () => {
+  return (
+    <div className='about-sevices-box'>
+      <div className='About-services'>
+        <div className='About-services-content'>
+          <h2 className='About-services-heading'>Transforming Spaces Transforming Lives</h2>
+          <div className='About-services-box'>
+            <div className='About-services-line'></div>
+            <div className='About-services-box-content'>
+              {/* image */}
+              <div className='About-services-image'></div>
+              <h5 className='About-services-title'>Plumbing</h5>
+              <p className='About-services-content'>Faucet change - Fixing leaks,Toilet Repairs and Replacement, Bidet installs - Dishwasher installs, - Upgrading old copper pipes to pex, - New shut-off valves, etc</p>
+            </div>
+          </div>
+          <div className='About-services-box'>
+            <div className='About-services-line'></div>
+            <div className='About-services-box-content'>
+              {/* image */}
+              <div className='About-services-image'></div>
+              <h5 className='About-services-title'>Electrical</h5>
+              <p className='About-services-content'>Light fictures - Power outlets - Switches, Decoration lights - TV Wall Mounting(hiiden wires)</p>
+            </div>
+          </div>
+          <div className='About-services-box'>
+            <div className='About-services-line'></div>
+            <div className='About-services-box-content'>
+              {/* image */}
+              <div className='About-services-image'></div>
+              <h5 className='About-services-title'>Washroom Renovation</h5>
+              <p className='About-services-content'>Tiling - WaterProofing, - New vanities, New mirrors - New accessories - Shower doors, Upgrading bath tubs to walk-in-showers</p>
+            </div>
+          </div>
+          <div className='About-services-box'>
+            <div className='About-services-line'></div>
+            <div className='About-services-box-content'>
+              {/* image */}
+              <div className='About-services-image'></div>
+              <h5 className='About-services-title'>HOUSE UPGRADES</h5>
+              <p className='About-services-content'>Building Decks/Patio or upgrading it - Drywall Repair -
+                Painting walls
+                Lock Changes - Door repairs and New Installs, Tenant Move in and out repairs, Cabinet Repairs, Appliance Installs, Junk Removal, Laminate and Tiles Ike Fixtures/Furniture Gazebo and Shed Installs, Kitchen and Washroom Caulking,Re-Grouting,Baseboards</p>
+            </div>
+          </div>
+          <div className='About-services-box'>
+            <div className='About-services-line'></div>
+            <div className='About-services-box-content'>
+              {/* image */}
+              <div className='About-services-image'></div>
+              <h5 className='About-services-title'>SECURITY SYSTEM</h5>
+              <p className='About-services-content'>
+                security and protection system, any of various means or devices to guard persons and property against a broad range of hazards, including crime, fire, accidents, espionage, sabotage, subversion, and attack.</p>
+            </div>
+          </div>
+          <div className='About-services-box'>
+            <div className='About-services-line'></div>
+            <div className='About-services-box-content'>
+              {/* image */}
+              <div className='About-services-image'></div>
+              <h5 className='About-services-title'>kitchen renovation</h5>
+              <p className='About-services-content'>Custom Cabinetry: We design and install custom cabinets to optimize storage space and add a touch of elegance to your kitchen.
+                Countertops and Backsplashes: Choose from a variety of high-quality materials for your countertops and backsplashes to suit your taste and lifestyle.
+                Appliance Installation: We ensure seamless integration of your appliances, making your kitchen efficient and user-friendly.
+                Lighting and Ventilation: Proper lighting and ventilation are crucial in any kitchen, and we provide solutions that enhance functionality and comfort.
+              </p>
+            </div>
+          </div>
+          <div className='About-services-box'>
+            <div className='About-services-line'></div>
+            <div className='About-services-box-content'>
+              {/* image */}
+              <div className='About-services-image'></div>
+              <h5 className='About-services-title'>bathroom renovation</h5>
+              <p className='About-services-content'> From conceptualization to completion, we take care of every detail, ensuring a seamless and hassle-free experience for you. Our team uses top-quality materials and innovative solutions to optimize space, functionality, and aesthetics. With our personalized approach and commitment to excellence, your dream bathroom is just a renovation away. Trust us to turn your vision into reality, and let us make your bathroom the envy of all who see it.
+              </p>
+            </div>
+          </div>
+          <div className='About-services-box'>
+            <div className='About-services-line'></div>
+            <div className='About-services-box-content'>
+              {/* image */}
+              <div className='About-services-image'></div>
+              <h5 className='About-services-title'>full remodels</h5>
+              <p className='About-services-content'>At our construction company, we offer comprehensive full remodel services that encompass every aspect of your home. Whether you want to transform a single room or give your entire property a fresh new look, we have the expertise and resources to handle it all. From concept to completion, we work closely with our clients to understand their vision and preferences, ensuring that the final result aligns perfectly with their needs.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutServices
